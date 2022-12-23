5cd74ba10e7ca98517fd125c138e795c4a59d5b9
Basimohd <107759928+Basimohd@users.noreply.github.com>
2022-12-23 16:42:26 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+jQuery.noConflict();
+jQuery(document).ready(function($){
+								
+							
+function lightboxPhoto() {
+	
+	jQuery("a[data-gal^='prettyPhoto']").prettyPhoto({
+			animationSpeed:'fast',
+			slideshow:5000,
+			theme:'light_rounded',
+			show_title:false,
+			overlay_gallery: false
+		});
+	
+	}
+	
+		if(jQuery().prettyPhoto) {
+	
+		lightboxPhoto(); 
+			
+	}
+	
+	
+if (jQuery().quicksand) {
+
+ 	// Clone applications to get a second collection
+	var $data = $(".portfolio-area").clone();
+	
+	//NOTE: Only filter on the main portfolio page, not on the subcategory pages
+	$('.portfolio-categ li').click(function(e) {
+		$(".filter li").removeClass("active");	
+		// Use the last category class as the category to filter by. This means that multiple categories are not supported (yet)
+		var filterClass=$(this).attr('class').split(' ').slice(-1)[0];
+		
+		if (filterClass == 'all') {
+			var $filteredData = $data.find('.portfolio-item2');
+		} else {
+			var $filteredData = $data.find('.portfolio-item2[data-type=' + filterClass + ']');
+		}
+		$(".portfolio-area").quicksand($filteredData, {
+			duration: 600,
+			adjustHeight: 'auto'
+		}, function () {
+
+				lightboxPhoto();
+						});		
+		$(this).addClass("active"); 			
+		return false;
+	});
+	
+}//if quicksand
+
+});
+//Click event handler for nav-items
+$('.nav-item').on('click',function(){
+
+	//Remove any previous active classes
+	$('.nav-item').removeClass('active');
+  
+	//Add active class to the clicked item
+	$(this).addClass('active');
+  });
+  
+// scroll active
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
